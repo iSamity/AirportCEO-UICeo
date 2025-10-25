a9fe95b2c760d0d463d0fbc6534a9c56082b75c2
iSamity <45425640+iSamity@users.noreply.github.com>
2025-10-25 20:13:38 +0200

    rename function and use correct function to load in main menu

--- QuickMainMenuLoading/QuickMainMenuLoading.cs
+++ QuickMainMenuLoading/QuickMainMenuLoading.cs
@@ -8,14 +8,17 @@ static class QuickMainMenuLoading
 
     [HarmonyPrefix]
     [HarmonyPatch(nameof(MainMenuWorldController.InvokeMainMenuLoadingAnimations))]
-    public static bool TestPrefix(MainMenuWorldController __instance)
+    public static bool Patch_InvokeMainMenuLoadingAnimations(MainMenuWorldController __instance)
     {
+        Plugin.Logger.LogInfo(Config.DefaultConfig.SkipLogosOnStartUp.Value);
         if (Config.DefaultConfig.SkipLogosOnStartUp.Value == false)
         {
             return true;
         }
 
-        __instance.StartCoroutine(__instance.LoadMainMenuFromGameWorld());
+
+        __instance.apoapsisStudiosLogoTransform.AttemptInvokeLoadSprites();
+        __instance.StartCoroutine(__instance.ShowHideGameMenuPanels(status: true, isLoadingFromDesktop: true));
 
         Singleton<AudioController>.Instance.InitializeAudio(launchInMainMenu: true);
 
